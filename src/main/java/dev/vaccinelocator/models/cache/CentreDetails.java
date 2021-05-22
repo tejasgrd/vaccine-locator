725898fc9cd167863e5b614bc34fd283211b9b5d
tejasgrd <tgarde@avaya.com>
2021-05-22 17:02:38 +0530

    Cache machenism updated

---
+++ src/main/java/dev/vaccinelocator/models/cache/CentreDetails.java
@@ -0,0 +1,15 @@
+package dev.vaccinelocator.models.cache;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CentreDetails {
+
+  private int centreId;
+  private int totalCapacity;
+}
